b4d79b8282bedbc0158b4951bc8e6909c6860292
Kunal267 <31859760+Kunal267@users.noreply.github.com>
2017-09-12 17:11:02 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		StudentGroup sg=new StudentGroup(5);
+		sg.toArray();
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	
+	}
+
+}
